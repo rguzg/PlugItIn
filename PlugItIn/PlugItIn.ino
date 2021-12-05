a32c6706a5da0550ed2f29d56f28aa93c509eb2d
Raúl <53493295+rguzg@users.noreply.github.com>
2021-12-05 07:55:08 -0600

    feat(WiFI): Add WiFi connection logic

---
+++ PlugItIn/PlugItIn.ino
@@ -0,0 +1,39 @@
+#include <ESP8266Wifi.h>
+#define OUTPUT_PIN D0
+
+const char *ssid = "Unova";
+const char *password = "luzbetra_internet!";
+
+WiFiClient client;
+
+void connectToWifi()
+{
+    Serial.println();
+    Serial.printf("Connecting to %s \n", ssid);
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(1000);
+        Serial.print(".");
+    }
+
+    Serial.printf("Connected to WiFi! %s \n", ssid);
+}
+
+void disconnectionHandler(const WiFiEventSoftAPModeStationDisconnected &)
+{
+    Serial.printf("Disconnected from %s \n", ssid);
+    connectToWifi();
+}
+
+void setup()
+{
+    Serial.begin(115200);
+    pinMode(OUTPUT_PIN, OUTPUT);
+    WiFi.onSoftAPModeStationDisconnected(&disconnectionHandler);
+
+    connectToWifi();
+}
