c4c30c87ca15fcdb5aa90ea2cc61646e3c16353e
Raúl <53493295+rguzg@users.noreply.github.com>
2021-12-05 14:58:26 -0600

    feat(MQTT): Add comunication with MQTT Server

--- PlugItIn/PlugItIn.ino
+++ PlugItIn/PlugItIn.ino
@@ -1,12 +1,19 @@
 #include <ESP8266WiFi.h>
-#define OUTPUT_PIN D0
+#include <PubSubClient.h>
+#define OUTPUT_PIN 5
 
 const char *ssid = "Unova";
 const char *password = "luzbetra_internet!";
+const char *mqtt_server = "broker.hivemq.com";
+
+void messageCallback(char *topic, byte *payload, unsigned int length);
 
 WiFiClient client;
 WiFiEventHandler connectedEventListener;
 WiFiEventHandler disconnectedEventListener;
+PubSubClient mqttClient = PubSubClient("broker.hivemq.com", 1883, *messageCallback, client);
+boolean mqttConnected = false;
+boolean mqttSubscribed = false;
 
 void connectToWifi()
 {
@@ -34,18 +41,77 @@ void connectionHandler(const WiFiEventStationModeConnected &event)
     Serial.printf("Connected to WiFi! %s \n", event.ssid);
 }
 
+void messageCallback(char *topic, byte *payload, unsigned int length)
+{
+
+    Serial.printf("Message arrived [%s] \n", topic);
+    for (int i = 0; i < length; i++)
+    {
+        Serial.print((char)payload[i]);
+    }
+    Serial.println((char)payload[0]);
+    Serial.println(length);
+    Serial.println(length == 1);
+
+    if (length == 1)
+    {
+        switch ((char)payload[0])
+        {
+        case '1':
+            Serial.println("Turning on the relay");
+            digitalWrite(OUTPUT_PIN, HIGH);
+            break;
+        case '2':
+            Serial.println("Turning off the relay");
+            digitalWrite(OUTPUT_PIN, LOW);
+            break;
+
+        default:
+            break;
+        }
+    }
+}
+
 void setup()
 {
     Serial.println("Setting up your device...");
     Serial.begin(115200);
+
     pinMode(OUTPUT_PIN, OUTPUT);
+    digitalWrite(OUTPUT_PIN, LOW);
+
     disconnectedEventListener = WiFi.onStationModeDisconnected(&disconnectionHandler);
     connectedEventListener = WiFi.onStationModeConnected(&connectionHandler);
+    mqttClient.setCallback(messageCallback);
 
     connectToWifi();
 }
 
 void loop()
 {
-    Serial.println("Looks like we're looping...");
+
+    if (!mqttConnected)
+    {
+        Serial.println("Connecting to MQTT server...");
+
+        mqttConnected = mqttClient.connect("B-0632");
+
+        if (mqttConnected)
+        {
+            Serial.println("Connected!");
+        }
+    }
+
+    if (!mqttSubscribed && mqttConnected)
+    {
+        Serial.println("Subscribing to topic...");
+        mqttSubscribed = mqttClient.subscribe("plugitin");
+
+        if (mqttSubscribed)
+        {
+            Serial.println("Connected!");
+        }
+    }
+
+    mqttClient.loop();
 }
