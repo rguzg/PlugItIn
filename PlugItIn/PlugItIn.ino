3bdf7b6c69855496ecb0b12be2337b53b7d46fd7
Raúl <53493295+rguzg@users.noreply.github.com>
2021-12-05 19:15:15 -0600

    Feat(Alarms): Return JSON with alarm history

--- PlugItIn/PlugItIn.ino
+++ PlugItIn/PlugItIn.ino
@@ -1,12 +1,17 @@
+#define ARDUINOJSON_USE_LONG_LONG 1
+
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
+#include <vector>
+#include <ArduinoJson.h>
 #define OUTPUT_PIN 5
 
 const char *ssid = "Unova";
 const char *password = "luzbetra_internet!";
 const char *mqtt_server = "broker.hivemq.com";
+std::vector<time_t> alarm_store;
 
 void messageCallback(char *topic, byte *payload, unsigned int length);
 
@@ -63,13 +68,39 @@ void messageCallback(char *topic, byte *payload, unsigned int length)
         {
         case '1':
             Serial.println("Turning on the relay");
+            alarm_store.push_back(timeClient.getEpochTime());
+            Serial.println(alarm_store.at(0));
             digitalWrite(OUTPUT_PIN, HIGH);
             break;
         case '2':
             Serial.println("Turning off the relay");
             digitalWrite(OUTPUT_PIN, LOW);
             break;
+        case '3':
+        {
+            Serial.println("Sending the alarm history");
+            StaticJsonDocument<JSON_ARRAY_SIZE(5) + JSON_OBJECT_SIZE(3)> response;
+            StaticJsonDocument<JSON_ARRAY_SIZE(5)> alarms;
 
+            JsonArray alarms_array = alarms.to<JsonArray>();
+
+            for (int i = 0; i < alarm_store.size(); i++)
+            {
+                alarms.add(alarm_store.at(i));
+            }
+
+            response["URL"] = "/alarms";
+            response["method"] = "GET";
+            response["alarms"] = alarms;
+
+            // Let's pray to god that 200 is a big enough buffer size!
+            char output[200];
+
+            serializeJson(response, output);
+            Serial.println(output);
+            mqttClient.publish("response", output);
+            break;
+        }
         default:
             break;
         }
@@ -94,6 +125,8 @@ void setup()
 
 void loop()
 {
+    timeClient.update();
+
     if (!mqttConnected)
     {
         Serial.println("Connecting to MQTT server...");
