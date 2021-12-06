19e6738c66649546077f25c46e76df513bca5bcd
Raúl <53493295+rguzg@users.noreply.github.com>
2021-12-06 01:27:13 -0600

    feat(API): Add GetAlarms

--- PlugItInApp/src/api/PlugItInAPI.ts
+++ PlugItInApp/src/api/PlugItInAPI.ts
@@ -89,4 +89,25 @@ export default class PlugItInAPI {
         return returnValue;
 
     }
+
+    GetAlarms(): Promise<Array<Number>>{
+
+        this.#MQTTClient.publish('plugitin', '{type: 3}');
+
+        let returnValue = new Promise<Array<Number>>((resolve, reject) => {
+            this.#MQTTClient.on("message", (topic: String, message:Uint8Array) => {
+                let parsed_message: PlugItInAPIResponseGET_ALARM = JSON.parse(message.toString());
+                if(topic == "response"){
+                    if(parsed_message.type == PlugItInAPIResponseType.GET_ALARM ){ 
+                        resolve(parsed_message.alarms);
+                    }
+                }
+
+                resolve([]);
+            });
+        });
+
+        return returnValue;
+
+    }
 }
